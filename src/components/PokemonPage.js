122823b6ca209e153d5ec2cdfc32c0d4b68bc38c
David Baden <davebaden@gmail.com>
2023-11-29 12:42:41 -0800

    done

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
+const pokemonAPI = "http://localhost:3001/pokemon";
+
 function PokemonPage() {
+
+  const [pokemon, setPokemon] = useState([]);
+  const [search, setSearch] = useState("");
+
+const filteredPokemon = pokemon.filter((poke) => {
+  return poke.name.toLowerCase().includes(search.toLowerCase());
+})
+
+function addNewItem(newPokemon) {
+  const updatedPokemon = [...pokemon, newPokemon];
+  setPokemon(updatedPokemon);
+}
+
+  useEffect (() => {
+    fetch(pokemonAPI)
+    .then((r) => r.json())
+    .then((pokemonArray) => {
+      setPokemon(pokemonArray);
+    })
+  }, [])
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm addNewItem={addNewItem} />
       <br />
-      <Search />
+      <Search setSearch={setSearch}/>
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemon={filteredPokemon} />
     </Container>
   );
 }
